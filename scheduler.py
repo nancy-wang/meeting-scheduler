ba7ab15506d6558abad6de7fbd45a5c94ce93ce4
Adit Bhatt <avbhatt@umich.edu>
2024-10-03 15:48:50 -0400

    feat: add fellow scheduling & multiple runs for optimization

--- scheduler.py
+++ scheduler.py
@@ -1,9 +1,9 @@
 import pandas as pd
-import random
 
 HISTORY_FILENAME = "pair_history.csv"
 LIMITATIONS_FILENAME = "limitations.csv"
 MAX_MEETINGS = 5
+OPTIMIZE_RUNS = 10
 
 class ValidationError(Exception):
     pass
@@ -24,20 +24,49 @@ def occupied_slots(availability_df, fellow):
     total_slots = len(availability_df[fellow])
     return total_slots - len(availability_df[fellow][(availability_df[fellow] == "True") | (availability_df[fellow] == "False")])
 
+def assign_coach_or_fellow(history_df, availability_df, max_meetings, coach_columns, coach_or_fellow):
+    available_fellows = history_df[coach_or_fellow].loc[(history_df[coach_or_fellow] == "False") & ~coach_columns].index
+    for fellow in available_fellows:
+        if occupied_slots(availability_df, fellow) < max_meetings:
+            slot, found = match_slots(availability_df[coach_or_fellow], availability_df[fellow])
+            if found:
+                availability_df.loc[slot, coach_or_fellow] = fellow
+                availability_df.loc[slot, fellow] = coach_or_fellow
+                history_df.loc[fellow, coach_or_fellow] = "True"
+                history_df.loc[coach_or_fellow, fellow] = "True"
+
+    return history_df, availability_df
+
 def assign_coaches(history_df, availability_df, max_meetings):
     coach_columns = availability_df.columns.str.startswith("[COACH]")
     for coach, _ in availability_df.loc[:, coach_columns].items():
-        available_fellows = history_df[coach].loc[(history_df[coach] == "False") & ~coach_columns].index
-        for fellow in available_fellows:
-            if occupied_slots(availability_df, fellow) < max_meetings:
-                slot, found = match_slots(availability_df[coach], availability_df[fellow])
-                if found:
-                    availability_df.loc[slot, coach] = fellow
-                    availability_df.loc[slot, fellow] = coach
-                    history_df.loc[fellow, coach] = "True"
-                    history_df.loc[coach, fellow] = "True"
+        assign_coach_or_fellow(history_df, availability_df, max_meetings, coach_columns, coach)
+
+def assign_fellows(history_df, availability_df, max_meetings):
+    coach_columns = availability_df.columns.str.startswith("[COACH]")
+    for fellow, _ in availability_df.loc[:, ~coach_columns].items():
+        assign_coach_or_fellow(history_df, availability_df, max_meetings, coach_columns, fellow)
 
     return history_df, availability_df
+
+def optimize_schedule(history_df, availability_df, max_meetings, optimize_runs):
+    optimal_history = history_df.copy()
+    optimal_availability = availability_df.copy()
+
+    optimal_slots = (optimal_availability.values == "True").sum()
+    for i in range(optimize_runs):
+        ongoing_availability = availability_df.copy()
+        ongoing_history = history_df.copy()
+        assign_coaches(ongoing_history, ongoing_availability, max_meetings)
+        assign_fellows(ongoing_history, ongoing_availability, max_meetings)
+        open_slots = (ongoing_availability.values == "True").sum()
+        if open_slots < optimal_slots:
+            print(i)
+            optimal_slots = open_slots
+            optimal_availability = ongoing_availability.copy()
+            optimal_history = ongoing_history.copy()
+
+    return optimal_history, optimal_availability
 
 def load_history(filename):
     history = pd.read_csv(filename, index_col=0,true_values=["X","x"]).fillna("False").replace(to_replace=True, value="True")
@@ -56,7 +85,7 @@ def main():
     # print(availability)
     # print("---")
 
-    assign_coaches(history, availability, MAX_MEETINGS)
+    history, availability = optimize_schedule(history, availability, MAX_MEETINGS, OPTIMIZE_RUNS)
 
     # print(availability)
 
--- test_scheduler.py
+++ test_scheduler.py
@@ -89,3 +89,138 @@ def test_assign_coaches_coach_galore_maxed():
     assert (availability['Luke'] == "[COACH]Anakin").any()
     assert (availability['Luke'] == "[COACH]Windu").any()
     assert not (availability['Luke'] == "[COACH]Palp").any()
+
+def test_assign_fellows():
+    history = scheduler.load_history('test/good_history.csv')
+    availability = scheduler.load_availability('test/limitations.csv')
+
+    scheduler.assign_fellows(history, availability, 5)
+
+    assert (availability['Anakin'] == "Obi").any()
+    assert (availability['Anakin'] == "Maul").any()
+    assert (availability['Luke'] == "Maul").any()
+    assert (availability['Obi'] == "Anakin").any()
+    assert (availability['Maul'] == "Luke").any()
+    assert (availability['Maul'] == "Anakin").any()
+
+    assert (history['Anakin']['Obi'])
+    assert (history['Anakin']['Maul'])
+    assert (history['Luke']['Maul'])
+    assert (history['Obi']['Anakin'])
+    assert (history['Maul']['Luke'])
+    assert (history['Maul']['Anakin'])
+
+def test_assign_fellows_fresh():
+    history = scheduler.load_history('test/fresh_history.csv')
+    availability = scheduler.load_availability('test/fresh_limits.csv')
+
+    scheduler.assign_fellows(history, availability, 5)
+
+    assert (availability['Anakin'] == "Obi").any()
+    assert (availability['Anakin'] == "Maul").any()
+    assert (availability['Anakin'] == "Luke").any()
+    assert (availability['Anakin'] == "Palp").any()
+    assert (availability['Luke'] == "Anakin").any()
+    assert (availability['Luke'] == "Obi").any()
+    assert (availability['Luke'] == "Maul").any()
+    assert (availability['Luke'] == "Palp").any()
+    assert (availability['Obi'] == "Anakin").any()
+    assert (availability['Obi'] == "Anakin").any()
+    assert (availability['Obi'] == "Luke").any()
+    assert (availability['Obi'] == "Maul").any()
+    assert (availability['Obi'] == "Palp").any()
+    assert (availability['Maul'] == "Luke").any()
+    assert (availability['Maul'] == "Anakin").any()
+    assert (availability['Maul'] == "Obi").any()
+    assert (availability['Maul'] == "Palp").any()
+
+    assert (history['Anakin']['Luke'])
+    assert (history['Anakin']['Obi'])
+    assert (history['Anakin']['Maul'])
+    assert (history['Anakin']['Palp'])
+    assert (history['Luke']['Anakin'])
+    assert (history['Luke']['Obi'])
+    assert (history['Luke']['Maul'])
+    assert (history['Luke']['Palp'])
+    assert (history['Obi']['Anakin'])
+    assert (history['Obi']['Luke'])
+    assert (history['Obi']['Maul'])
+    assert (history['Obi']['Palp'])
+    assert (history['Maul']['Anakin'])
+    assert (history['Maul']['Luke'])
+    assert (history['Maul']['Obi'])
+    assert (history['Maul']['Palp'])
+    assert (history['Maul']['Anakin'])
+
+def test_assign_coaches_and_fellows_fresh():
+    history = scheduler.load_history('test/fresh_history.csv')
+    availability = scheduler.load_availability('test/fresh_limits.csv')
+
+    scheduler.assign_coaches(history, availability, 5)
+    scheduler.assign_fellows(history, availability, 5)
+
+    # coaches take priority
+    assert (availability['[COACH]Yoda'] == "Maul").any()
+    assert (availability['Maul'] == "[COACH]Yoda").any()
+    assert (availability['[COACH]Yoda'] == "Anakin").any()
+    assert (availability['Anakin'] == "[COACH]Yoda").any()
+    assert (availability['[COACH]Yoda'] == "Obi").any()
+    assert (availability['Obi'] == "[COACH]Yoda").any()
+    assert (availability['[COACH]Yoda'] == "Palp").any()
+    assert (availability['Palp'] == "[COACH]Yoda").any()
+    assert (availability['[COACH]Yoda'] == "Luke").any()
+    assert (availability['Luke'] == "[COACH]Yoda").any()
+
+    assert (availability['[COACH]Windu'] == "Maul").any()
+    assert (availability['Maul'] == "[COACH]Windu").any()
+    assert (availability['[COACH]Windu'] == "Anakin").any()
+    assert (availability['Anakin'] == "[COACH]Windu").any()
+    assert (availability['[COACH]Windu'] == "Obi").any()
+    assert (availability['Obi'] == "[COACH]Windu").any()
+    assert (availability['[COACH]Windu'] == "Palp").any()
+    assert (availability['Palp'] == "[COACH]Windu").any()
+    assert (availability['[COACH]Windu'] == "Luke").any()
+    assert (availability['Luke'] == "[COACH]Windu").any()
+
+    # each fellow meets with at least 2 other fellows
+    assert availability['Anakin'].isin(["Obi","Maul","Luke","Palp"]).sum() >= 2
+    assert availability['Obi'].isin(["Anakin","Maul","Luke","Palp"]).sum() >= 2
+    assert availability['Maul'].isin(["Obi","Anakin","Luke","Palp"]).sum() >= 2
+    assert availability['Luke'].isin(["Obi","Maul","Anakin","Palp"]).sum() >= 2
+    assert availability['Palp'].isin(["Obi","Maul","Luke","Anakin"]).sum() >= 2
+
+def test_optimize_schedule():
+    history = scheduler.load_history('test/fresh_history.csv')
+    availability = scheduler.load_availability('test/fresh_limits.csv')
+
+    history, availability = scheduler.optimize_schedule(history, availability, 5, 10)
+
+    # coaches take priority
+    assert (availability['[COACH]Yoda'] == "Maul").any()
+    assert (availability['Maul'] == "[COACH]Yoda").any()
+    assert (availability['[COACH]Yoda'] == "Anakin").any()
+    assert (availability['Anakin'] == "[COACH]Yoda").any()
+    assert (availability['[COACH]Yoda'] == "Obi").any()
+    assert (availability['Obi'] == "[COACH]Yoda").any()
+    assert (availability['[COACH]Yoda'] == "Palp").any()
+    assert (availability['Palp'] == "[COACH]Yoda").any()
+    assert (availability['[COACH]Yoda'] == "Luke").any()
+    assert (availability['Luke'] == "[COACH]Yoda").any()
+
+    assert (availability['[COACH]Windu'] == "Maul").any()
+    assert (availability['Maul'] == "[COACH]Windu").any()
+    assert (availability['[COACH]Windu'] == "Anakin").any()
+    assert (availability['Anakin'] == "[COACH]Windu").any()
+    assert (availability['[COACH]Windu'] == "Obi").any()
+    assert (availability['Obi'] == "[COACH]Windu").any()
+    assert (availability['[COACH]Windu'] == "Palp").any()
+    assert (availability['Palp'] == "[COACH]Windu").any()
+    assert (availability['[COACH]Windu'] == "Luke").any()
+    assert (availability['Luke'] == "[COACH]Windu").any()
+
+    # each fellow meets with at least 2 other fellows
+    assert availability['Anakin'].isin(["Obi","Maul","Luke","Palp"]).sum() >= 2
+    assert availability['Obi'].isin(["Anakin","Maul","Luke","Palp"]).sum() >= 2
+    assert availability['Maul'].isin(["Obi","Anakin","Luke","Palp"]).sum() >= 2
+    assert availability['Luke'].isin(["Obi","Maul","Anakin","Palp"]).sum() >= 2
+    assert availability['Palp'].isin(["Obi","Maul","Luke","Anakin"]).sum() >= 2
